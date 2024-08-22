8759812f4a9ad8f202585942a059c65478ee8e52
Jineeth <jin123vijayan3@gmail.com>
2024-08-22 22:46:55 +0530

    trail-7

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+console.log(import.meta.env.VITE_API_URL);
+
 const axiosInstance = axios.create({
-  baseURL:` ${import.meta.env.VITE_API_URL}/api/v1`, // Replace with your API's base URL
-  withCredentials: true, // This ensures cookies are sent with requests
+  baseURL:`${import.meta.env.VITE_API_URL}/api/v1`,
+  withCredentials: true,
 });
 
 export default axiosInstance;
