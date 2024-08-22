4034c28b0790486a8e5df001c3dc4752796cd80a
Jineeth <jin123vijayan3@gmail.com>
2024-08-22 21:49:02 +0530

    5th final commit

--- src/components/guest/GuestTurf.jsx
+++ src/components/guest/GuestTurf.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import GuestCards from './GuestCards';
+import axiosInstance from '../../config/axiosInstance.js';
 
 
 
@@ -14,7 +15,7 @@ const GuestTurf = () => {
         const getAllTurfs = async () => {
             try {
 
-                const res = axios.get('http://localhost:3001/api/v1/turfs/get-limited-turf');
+                const res = axiosInstance.get('/turfs/get-limited-turf');
 
                 const data = (await res).data;
 
